202c733e1dbf92e63ed86bf429c0dead3377101f
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-03 02:04:23 +0900

    Fix: 함수명 변경

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,5 @@ const promise = str2hex.receiveStr('HOW');
 
 promise
     .then(str2hex.str2arr)
-    .then(str2hex.char2dec)
-    .then(str2hex.dec2hex)
+    .then(str2hex.charArr2decArr)
+    .then(str2hex.decArr2hexArr)
--- public/js/str2hexArr.js
+++ public/js/str2hexArr.js
@@ -2,13 +2,13 @@ const receiveStr = str => new Promise((resolve, reject) => {resolve(str)});
 
 const str2arr = str => str.replace(' ', '').split('');
 
-const char2dec = charArr => charArr.map(char => char.charCodeAt(0));
+const charArr2decArr = charArr => charArr.map(char => char.charCodeAt(0));
 
-const dec2hex = decArr => decArr.map(dec => dec.toString(16)); 
+const decArr2hexArr = decArr => decArr.map(dec => dec.toString(16)); 
 
 export default {
     receiveStr,
     str2arr,
-    char2dec,
-    dec2hex
+    charArr2decArr,
+    decArr2hexArr
 }
