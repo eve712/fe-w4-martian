c9ed71756508879758ba194d5f649f3a9656df43
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-03 16:27:29 +0900

    Fix: 화살표 방향 돌리는 로직 잘못 짜서 완전 수정
    * 화살표의 현재 위치에서 목표 위치의 가까운 방향으로 회전하려면 절댓값으로 계산해야 한다.
    * 목표 위치의 각도는 양수, 음수의 값이 있는데 이 두 가지 각도와 현재의 각도의 절댓값의차이를 구한다.
    * 그 차이가 작은 것이 가까운 각도이다. 그래서 작은 값을 움직일 각도로 정하고 회전시켰다.
    * 이전의 방법은 시계, 반시계 방향 중 가까운 방향을 구하고 반시계면 무조건 -각도로 했는데 이는 잘못된 방법이다.
    * -여도 절대값이 차이가 많이 난다면 삥 돌아서 회전한다

--- public/js/rotateArrow.js
+++ public/js/rotateArrow.js
@@ -1,36 +1,27 @@
 const $arrow = document.querySelector('.table__arrow');
 
+// getArrowAngle에서 각도를 받아와서 회전시키는 함수
 const rotateArrow = (_curr, _des) => {
-    let angle;
     const [curr, des] = [parseInt(_curr, 16), parseInt(_des, 16)];
-    if (getDirection(curr, des) === 'clock') {
-        angle = getClockWiseAngle(des)
-    }
-    else if(getDirection(curr, des) === 'counter') {
-        angle = getClockWiseAngle(des) - 360;
-    }
+    const angle = getArrowAngle(curr, des);
     $arrow.style.transform = `rotate(${angle}deg)`
 }
 
-const getDirection = (curr, des) => {
-    let direction;
-    const [diff1, diff2] = [des - curr, (16 - des) + curr];
-    if(curr <= des) direction = diff1 < diff2 ? 'clock' : 'counter'
-    else direction = diff1 < diff2 ? 'counter' : 'clock'
-    return direction;
+// 화살표의 현재 위치와 목표위치를 계산해서 빠른 방향의 각도를 알아내는 함수
+const getArrowAngle = (curr, des) => {
+    const currAngle = getAngle(curr);
+    const [angle1, angle2] = [getAngle(des), getAngle(des) - 360];
+
+    const diff1 = Math.abs(Math.abs(currAngle) - Math.abs(angle1));
+    const diff2 = Math.abs(Math.abs(currAngle) - Math.abs(angle2));
+    const realAngle = diff1 > diff2 ? angle2 : angle1;
+    return realAngle;
 }
 
-const getClockWiseAngle = num => {
-    if(num >= 4) return getArrowAngle(num)
-    else {
-        num += 16;
-        return getArrowAngle(num)
-    }
-}
-
-const getArrowAngle = num => {
+const getAngle = num => {
     const oneAngle = 22.5;
+    if(num < 4) num += 16;
     return oneAngle / 2 + (num - 4) * oneAngle
 }
 
-export default rotateArrow
+export default rotateArrow;
