6880bbdefc37fbd370637b53bee8b70845c74ac0
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-03 19:28:19 +0900

    Fix: 화살표의 현재 위치의 각도를 구하는 방법 변경
    * getAngle()로 구하면 무조건 양수의 각도만 나오기 때문에 음수일 경우 정반대로 회전하는 상황 발생
    * 화살표 요소의 attribute 값을 가져와 현재 입력돼있는 각도를 가져와서 해결함.

--- public/js/rotateArrow.js
+++ public/js/rotateArrow.js
@@ -9,7 +9,8 @@ const rotateArrow = (_curr, _des) => {
 
 // 화살표의 현재 위치와 목표위치를 계산해서 빠른 방향의 각도를 알아내는 함수
 const getArrowAngle = (curr, des) => {
-    const currAngle = getAngle(curr);
+    const style = $arrow.getAttribute('style');
+    const currAngle = (style === null) ? 0 : style.slice(18, -5)
     const [angle1, angle2] = [getAngle(des), getAngle(des) - 360];
 
     const diff1 = Math.abs(Math.abs(currAngle) - Math.abs(angle1));
