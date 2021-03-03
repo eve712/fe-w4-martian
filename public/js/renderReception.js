af1207b45ebacac1f2eacdcfeaa9544cea4090a4
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-03 17:42:05 +0900

    Refactor: renderReception함수 쪼개기

--- public/js/renderReception.js
+++ public/js/renderReception.js
@@ -20,29 +20,36 @@ const $txt = {
     'F' : document.querySelector('.txt_F')
 }
 
-const after2sec = () => new Promise((resolve) => setTimeout(() => resolve('result'), 2000)) 
-const after5sec = () => new Promise((resolve) => setTimeout(() => resolve('result'), 5000)) 
-const after400ms = () => new Promise((resolve) => setTimeout(() => resolve('result'), 400)) 
+const after2sec = () => new Promise((resolve) => setTimeout(() => resolve(), 2000)) 
+const after5sec = () => new Promise((resolve) => setTimeout(() => resolve(), 5000)) 
+const after400ms = () => new Promise((resolve) => setTimeout(() => resolve(), 400)) 
 
 const renderReception = async function (hexArr) {
     let curr = 4;
     for(let i = 0; i < hexArr.length; i++) {
         const hex = hexArr[i];
-        rotateArrow(curr, hex[0]);
+        render(curr, hex, true)
         curr = hex[0]
-        await after400ms();
-        $receptionBox.innerHTML += `${curr}`;
-        $txt[hex[0]].style.color = 'rgb(245, 108, 53)';
-        await after2sec();
-        $txt[hex[0]].removeAttribute('style');
-        rotateArrow(curr, hex[1]);
+        await after2sec();  // ======= 2초 쉬고
+        render(curr, hex, false)
         curr = hex[1]
-        await after400ms();
-        $receptionBox.innerHTML += `${curr} `;
-        $txt[hex[1]].style.color = 'rgb(245, 108, 53)';
-        await after5sec();
+        await after5sec();  // ======= 넘어가기 전 5초 쉬고
         $txt[hex[1]].removeAttribute('style');
     }
+}
+
+const render = (curr, hex, isFirst) => {
+    if(!isFirst) $txt[hex[0]].removeAttribute('style');
+    const destination = isFirst ? hex[0] : hex[1];
+    rotateArrow(curr, destination);
+    printInfo(destination, isFirst)
+}
+
+const printInfo = async function (curr, isFirst) {
+    await after400ms();
+    if(isFirst) $receptionBox.innerHTML += `${curr}`;
+    else $receptionBox.innerHTML += `${curr} `;
+    $txt[curr].style.color = 'rgb(245, 108, 53)';
 }
 
 
